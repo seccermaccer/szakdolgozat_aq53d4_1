c90fe5f4f880161ddba0091e0ba280a098b39c13
Kovács Sándor <h047494@stud.u-szeged.hu>
2023-01-09 20:06:51 +0100

    login update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'elektroworld-f4a4a',
+    appId: '1:685889117460:web:b87528279ff80549543601',
+    storageBucket: 'elektroworld-f4a4a.appspot.com',
+    apiKey: 'AIzaSyCbBFFUAHlAAvVyuBtOAddUrwcQegJNbsE',
+    authDomain: 'elektroworld-f4a4a.firebaseapp.com',
+    messagingSenderId: '685889117460',
+    measurementId: 'G-6HSFRMV8RT',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'elektroworld-f4a4a',
+    appId: '1:685889117460:web:b87528279ff80549543601',
+    storageBucket: 'elektroworld-f4a4a.appspot.com',
+    apiKey: 'AIzaSyCbBFFUAHlAAvVyuBtOAddUrwcQegJNbsE',
+    authDomain: 'elektroworld-f4a4a.firebaseapp.com',
+    messagingSenderId: '685889117460',
+    measurementId: 'G-6HSFRMV8RT',
+  },
   production: false
 };
 
