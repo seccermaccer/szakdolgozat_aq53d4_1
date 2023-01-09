d410e26e6b6e2b567c0d5ba09d22d78ade318ba7
Kovács Sándor <h047494@stud.u-szeged.hu>
2023-01-09 20:22:13 +0100

    loginasd

--- src/app/bejelentkezes/bejelentkezes.component.ts
+++ src/app/bejelentkezes/bejelentkezes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../shared/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-bejelentkezes',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BejelentkezesComponent implements OnInit {
 
-  constructor() { }
+  loginForm = new FormGroup({
+    email: new FormControl('',[Validators.required,Validators.email]),
+    password: new FormControl('',Validators.required)
+  })
+
+  constructor( private authService: AuthService,
+               private router: Router,) { }
 
   ngOnInit(): void {
+  }
+
+  get email(){
+    return this.loginForm.get('email');
+  }
+
+  get password(){
+    return this.loginForm.get('password');
+  }
+
+  submit(){
+    if (!this.loginForm.valid){
+      return;
+    }
+
+    const {email,password} = this.loginForm.value;
+    this.authService.login(<string>email,<string>password).subscribe(() => {
+      this.router.navigate(['/profil']);
+    })
+
   }
 
 }
