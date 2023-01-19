da7c3fa20f5270ea2695f725c2c687f3a4581c5a
Kovács Sándor <sandor.kovacs@inclouded.hu>
2023-01-19 18:13:35 +0100

    page 1

--- src/app/shop-menu/shop-menu.component.ts
+++ src/app/shop-menu/shop-menu.component.ts
@@ -10,6 +10,13 @@ export class ShopMenuComponent implements OnInit {
   categories: any;
   categoryImages: any;
   categoryImages2: any;
+  categoryImages3: any;
+  categoryImages4: any;
+  categoryImages5: any;
+  categoryImages6: any;
+  categoryImages7: any;
+  categoryImages8: any;
+
 
 
   constructor(private authS: AuthService) { }
@@ -19,6 +26,12 @@ export class ShopMenuComponent implements OnInit {
       this.categories = data;
       this.categoryImages = this.categories.map((category: { imageUrl: any; }) => category.imageUrl);
       this.categoryImages2 = this.categories.map((category: { imageUrl2: any; }) => category.imageUrl2);
+      this.categoryImages3 = this.categories.map((category: { imageUrl3: any; }) => category.imageUrl3);
+      this.categoryImages4 = this.categories.map((category: { imageUrl4: any; }) => category.imageUrl4);
+      this.categoryImages5 = this.categories.map((category: { imageUrl5: any; }) => category.imageUrl5);
+      this.categoryImages6 = this.categories.map((category: { imageUrl6: any; }) => category.imageUrl6);
+      this.categoryImages7 = this.categories.map((category: { imageUrl7: any; }) => category.imageUrl7);
+      this.categoryImages8 = this.categories.map((category: { imageUrl8: any; }) => category.imageUrl8);
     });
   }
 
