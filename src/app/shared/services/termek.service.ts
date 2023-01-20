541e4e9a24d5097deab53478be52c55ec840c04f
Kovács Sándor <sandor.kovacs@inclouded.hu>
2023-01-20 13:21:59 +0100

    kategóriánként termékek listázása

--- src/app/shared/services/termek.service.ts
+++ src/app/shared/services/termek.service.ts
@@ -15,4 +15,28 @@ export class TermekService {
   getProductsByCategory2(category: string) {
     return this.db.collection('Products', ref => ref.where('category', '==', category)).valueChanges();
   }
+
+  getProductsByCategory3(category: string) {
+    return this.db.collection('Products', ref => ref.where('category', '==', category)).valueChanges();
+  }
+
+  getProductsByCategory4(category: string) {
+    return this.db.collection('Products', ref => ref.where('category', '==', category)).valueChanges();
+  }
+
+  getProductsByCategory5(category: string) {
+    return this.db.collection('Products', ref => ref.where('category', '==', category)).valueChanges();
+  }
+
+  getProductsByCategory6(category: string) {
+    return this.db.collection('Products', ref => ref.where('category', '==', category)).valueChanges();
+  }
+
+  getProductsByCategory7(category: string) {
+    return this.db.collection('Products', ref => ref.where('category', '==', category)).valueChanges();
+  }
+
+  getProductsByCategory8(category: string) {
+    return this.db.collection('Products', ref => ref.where('category', '==', category)).valueChanges();
+  }
 }
--- src/app/shop-menu/termekek/termekek.component.ts
+++ src/app/shop-menu/termekek/termekek.component.ts
@@ -11,6 +11,12 @@ export class TermekekComponent implements OnInit {
 
   products: any[] | undefined;
   products2: any[] | undefined;
+  products3: any[] | undefined;
+  products4: any[] | undefined;
+  products5: any[] | undefined;
+  products6: any[] | undefined;
+  products7: any[] | undefined;
+  products8: any[] | undefined;
   selectedProductId: number | undefined;
   showProducts: boolean | undefined;
 
@@ -23,6 +29,30 @@ export class TermekekComponent implements OnInit {
 
     this.termekService.getProductsByCategory2("Számítógép Periféria").subscribe(products => {
       this.products2 = products;
+    });
+
+    this.termekService.getProductsByCategory3("Szórakoztató Elektronika").subscribe(products => {
+      this.products3 = products;
+    });
+
+    this.termekService.getProductsByCategory4("TV,monitor").subscribe(products => {
+      this.products4 = products;
+    });
+
+    this.termekService.getProductsByCategory5("Mobil,tablet,kamera").subscribe(products => {
+      this.products5 = products;
+    });
+
+    this.termekService.getProductsByCategory6("Háztartási gép,eszköz").subscribe(products => {
+      this.products6 = products;
+    });
+
+    this.termekService.getProductsByCategory7("Okosotthon").subscribe(products => {
+      this.products7 = products;
+    });
+
+    this.termekService.getProductsByCategory8("Számítógép tartozékok").subscribe(products => {
+      this.products8 = products;
     });
 
     this.selectedProductId = this.productService.getSelectedProductId();
